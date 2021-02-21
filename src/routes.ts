ed1d1d1f5d826b0d54cadeb4dd5cac8730138f36
Marcelino <marceometry@gmail.com>
2021-02-21 03:31:13 -0300

    routes att

--- src/routes.ts
+++ src/routes.ts
@@ -7,7 +7,12 @@ const routes = Router()
 const upload = multer(uploadConfig)
 
 routes.get('/orphanages', orphanagesController.index)
+routes.get('/admin/registered', orphanagesController.index)
+routes.get('/admin/pending', orphanagesController.index)
 routes.get('/orphanages/:id', orphanagesController.show)
+routes.get('/admin/delete/:id', orphanagesController.show)
+routes.get('/admin/edit/:id', orphanagesController.show)
+routes.post('/admin/edit/:id', orphanagesController.create)
 routes.post('/orphanages', upload.array('images'), orphanagesController.create)
 
 export default routes
